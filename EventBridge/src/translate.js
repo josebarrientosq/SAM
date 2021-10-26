34d64af1e77946e719ea2263c8032bb9c50149a2
Jose Barrientos <josebarrientosq@gmail.com>
2021-10-26 12:38:03 -0500

    add kinesis firehose

---
+++ EventBridge/src/translate.js
@@ -0,0 +1,19 @@
+const AWS = require('aws-sdk');
+const translate = new AWS.Translate();
+
+exports.lambdaHandler = async (event) => {
+  try {
+    let params = {
+      SourceLanguageCode: "auto",
+      TargetLanguageCode: event.detail.language,
+      Text: event.detail.data
+    }
+    let translation = await translate.translateText(params).promise()
+    console.log(translation.TranslatedText)
+  } catch (err) {
+    console.log(err.message)
+  }
+  return {
+    "success": true
+  }
+}
---
+++ sam-eventbridge-order/ThaiLand/handler.js
@@ -0,0 +1,5 @@
+exports.thaiLand = async (event) => {
+    console.log('restaurant ThaiLand recieved an order');
+    console.log(event);
+    return;
+}
